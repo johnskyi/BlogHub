8cb31b22ddbecde7c3dddf5d5381be2d123f94d5
johnskyi <johnskyi2@yandex.ru>
2021-09-01 18:07:06 +0300

    Initial commit

---
+++ src/main/java/com/example/BlogHub/BlogHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.BlogHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BlogHub/BlogHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BlogHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
